69346ad6600c873e665dc3c3f64da06de8290fdc
diego <diegozaratean@gmail.com>
2026-01-14 11:30:59 +0000

    finish data modeling review

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -15,5 +16,78 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Consola(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    precio: Mapped[int] = mapped_column(nullable=False)
+
+    consola_videojuegos: Mapped[List["ConsolaVideojuego"]] = relationship(back_populates="consola")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Empresa(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120))
+    ciudad: Mapped[str] = mapped_column(String(120))
+    slogan: Mapped[str] = mapped_column(String(120))
+
+    videojuegos: Mapped[List["Videojuego"]] = relationship(back_populates="empresa")
+
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
+class Videojuego(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
+
+    empresa_id: Mapped[int] = mapped_column(ForeignKey("empresa.id"))
+    empresa: Mapped["Empresa"] = relationship(back_populates="videojuegos")
+
+    consola_videojuegos: Mapped[List["ConsolaVideojuego"]] = relationship(back_populates="videojuego")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+class ConsolaVideojuego(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    consola_id: Mapped[int] = mapped_column(ForeignKey("consola.id"))
+    consola: Mapped["Consola"] = relationship(back_populates="consola_videojuegos")
+
+    videojuego_id: Mapped[int] = mapped_column(ForeignKey("videojuego.id"))
+    videojuego: Mapped["Videojuego"] = relationship(back_populates="consola_videojuegos")
+    
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
             # do not serialize the password, its a security breach
         }
